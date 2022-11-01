6979747f396bfa32ef2df5e3ddd16577dc8c237c
設樂直希 <yatta5537@gmail.com>
2022-11-01 09:46:13 +0000

    third

--- resources/views/admin/myapp/eventdetail.blade.php
+++ resources/views/admin/myapp/eventdetail.blade.php
@@ -15,7 +15,7 @@
                         </ul>
                     @endif
                     
-                   <p><div class="form-group row">
+                   <div class="form-group row">
                         <label class="col-md-2">種目</label>
                         <div class="col-md-4">
                             
@@ -26,28 +26,30 @@
                              </select>
                              <a href="https://cb8773d8e47445c68b8cdd1855a50870.vfs.cloud9.us-east-2.amazonaws.com/admin/myapp/event_name">その他の種目の登録はこちら</a>
                         </div>
-                    </div></p>
-                    <p><div class="form-group row">
+                    </div>
+                    <div class="form-group row">
                         <label class="col-md-2">重さ</label>
                         <div class="col-md-2">
                             <input type="text" class="form-control" name="weight" value="{{ old('weight') }}">
                         </div>
-                    </div></p>
-                     <p><div class="form-group row">
+                    </div>
+                       <div class="form-group row">
                        <label class="col-md-2">回数</label>
                         <div class="col-md-2">
                             <input type="text" class="form-control" name="reps" value="{{ old('reps') }}">
                         </div>
-                    </div></p>
-                   <p><div class="form-group row">
-                    　<label class="col-md-2">セット</label>
-                    　<div class="col-md-2">
+                    </div>
+                    <div class="form-group row">
+                       <label class="col-md-2">セット</label>
+                        <div class="col-md-2">
                             <input type="text" class="form-control" name="sets" value="{{ old('sets') }}">
-                    　</div>
-                    </div></p>
-                   @csrf
-                   <p><input type="submit" class="btn btn-primary" value="追加"></p>
+                        </div>
+                    </div>
+                    
+                    @csrf
+                   <input type="submit" class="btn btn-primary" value="追加">
                 </form>
+                
                 <input type="button"  class="btn btn-primary" onclick="location.href='https://cb8773d8e47445c68b8cdd1855a50870.vfs.cloud9.us-east-2.amazonaws.com/admin/myapp/main'"value="カレンダーに戻る">
                 <input type="button"  class="btn btn-primary" onclick="location.href='https://cb8773d8e47445c68b8cdd1855a50870.vfs.cloud9.us-east-2.amazonaws.com/admin/myapp/event'"value="種目名を登録">
                 <input type="button"  class="btn btn-primary" onclick="location.href='https://cb8773d8e47445c68b8cdd1855a50870.vfs.cloud9.us-east-2.amazonaws.com/admin/myapp/record_menu'"value="カレンダーに記録する">
