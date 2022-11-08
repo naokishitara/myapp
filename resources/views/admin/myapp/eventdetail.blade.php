a68e6d609e9a1079f1eb9726076ce7a0786c3500
設樂直希 <yatta5537@gmail.com>
2022-11-08 09:00:37 +0000

    deployしました11

--- resources/views/admin/myapp/eventdetail.blade.php
+++ resources/views/admin/myapp/eventdetail.blade.php
@@ -50,7 +50,7 @@
                         <input type="submit" class="btn btn-primary" value="追加">
                    </div>
                 </form>
-                <input type="button"  class="btn btn-primary" onclick="location.href='https://shielded-depths-42794.herokuapp.com/admin/myapp/eventdetail'"value="種目名を登録">
+                <input type="button"  class="btn btn-primary" onclick="location.href='https://shielded-depths-42794.herokuapp.com/admin/myapp/event'"value="種目名を登録">
                 <input type="button"  class="btn btn-primary" onclick="location.href='https://shielded-depths-42794.herokuapp.com/admin/myapp/record_menu'"value="カレンダーに記録">
                 <input type="button"  class="btn btn-primary" onclick="location.href='https://shielded-depths-42794.herokuapp.com/admin/myapp/main'"value="カレンダーに戻る">
             </div>
